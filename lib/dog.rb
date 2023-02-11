655a0dea39373526fd3da59f36917e61d01897f0
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-11 23:38:26 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-
-end
+    #adding two methods to the Dog class
+    #setting an individual dog's name
+ def name=(dog_name)
+    @this_dogs_name = dog_name
+ end
+ #getting the individual dog's name
+ def name
+    @this_dogs_name
+ end
+end 
+lassie = Dog.new
+lassie.name = "Lassie"
